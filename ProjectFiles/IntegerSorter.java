05c3fc52a21b9c5967c7bc3bceb81ee484598e9d
Ian Hecker <ianhecker@yahoo.com>
2017-02-27 14:39:08 -0700

    Update IntegerSorter.java

--- ProjectFiles/IntegerSorter.java
+++ ProjectFiles/IntegerSorter.java
@@ -6,5 +6,6 @@ package binpacking;
  */
 public class IntegerSorter
 {
-
+  //Do we need an integer sorter? Why not use another MaxPQ? It would be logN 
+  //time to add and find max. log(100,000) = 5. So adding 100,000 elements is >= 5 seconds-ish
 }
