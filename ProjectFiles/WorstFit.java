cfb82d80b3af2735ba16c33df3bd2200afff0216
Ian Hecker <ianhecker@yahoo.com>
2017-02-27 14:36:04 -0700

    Update WorstFit.java

--- ProjectFiles/WorstFit.java
+++ ProjectFiles/WorstFit.java
@@ -6,8 +6,34 @@ package binpacking;
  */
 public class WorstFit
 {
-    public static void main(String[] args)
-    {
+    //int[] dataArray;
+    //MaxPQ PQ;
     
+    public static void main(String[] args)
+    {       
+        //Take input
+        
+        //Input into IntegerSorter.java --> Largest first
+
+        //Initialize Priority Queue for disks
+            //Make a PQ for ints as well??
+        
+        //Call sorting method
+    }
+    public void worstFitSort()
+    {
+        /* Create initial Disk
+         * Place Disk into PQ
+         * Place data in disk
+         * 
+         * IF (next dataFile fits onto Disk)  {add}
+         * ELSE IF (dataFile fits on any other Disk in PQ)  {add}
+         * ELSE {create new disk, add dataFile onto Disk_2}
+         * End loop once dataFile inputs have all been placed
+         * 
+         * //Maybe we can put integers into a Priortiy Queue as well? 
+         * //Then we dont need an IntegerSorter.java and can remove an int from the intPQ
+         * //when it has been placed
+         */
     }
 }
