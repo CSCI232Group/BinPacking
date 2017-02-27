ebef3502dda4cf9a82abde9365debc2a863bf404
Ian Hecker <ianhecker@yahoo.com>
2017-02-27 14:56:39 -0700

    Update IntegerSorter.java

--- ProjectFiles/IntegerSorter.java
+++ ProjectFiles/IntegerSorter.java
@@ -8,6 +8,16 @@ public class IntegerSorter
 {
   //Do we need an integer sorter? Why not use another MaxPQ? It would be logN 
   //time to add elements. log(100,000) = 5. So adding 100,000 elements is >= 5 seconds-ish
-  //Considering the MaxPQ handled the particles in Lab1 very well, I propose we attempt to use
+  //Considering the MaxPQ handled the particles in Lab1 very well, maybe we can use
   //the MaxPQ to also sort the integers
+  
+  //!!!!!!!!!!!!!!!!!!
+  //Actually, using QuickSort and an ArrayList would be easier to implement than using MaxPQ and
+  //and creating an object for each integer. Then when we add a dataFile to a Disk, we can delete
+  //it from the ArrayList or:
+  //
+  //int currentItem = intList.first();
+  //**add dataFile to Disk**
+  //currentItem = currentItem.next();
+  //!!!!!!!!!!!!!!!!!!
 }
