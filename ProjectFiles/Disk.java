c220d292a0d00c2763beff95acb07e565adf9170
Ian Hecker <ianhecker@yahoo.com>
2017-03-01 14:43:56 -0700

    Add files via upload

--- ProjectFiles/Disk.java
+++ ProjectFiles/Disk.java
@@ -4,29 +4,59 @@ package binpacking;
  *
  * @author Ian
  */
-public class Disk implements Comparable<Disk> //May be "extends" vs implements
+public class Disk implements Comparable<Disk> //????May be "extends" vs implements????
 {
     int totalStorage;
     int usedStorage;
-    //int[] fileArray;
+    Node firstFile;
+    Node currentFile;
     
     public Disk()
-    {
+    {        
         totalStorage = 1000000;
-        usedStorage = totalStorage;
+        usedStorage = 0;
+        firstFile = null;        
     }
-    public void fill(int size)
+    public void fill(int dataFile)
     {
-        if(totalStorage - usedStorage - size >= 0)
-        {usedStorage += size;}
-        //add file into disk record - array???
+        if(usedStorage + dataFile <= totalStorage)
+        {
+            usedStorage += dataFile;//add dataFile to used space
+        
+            if(firstFile == null)//If this is the first dataFile
+            {
+                firstFile = new Node(dataFile, null); //Creates first Node
+                currentFile = firstFile; //Sets currentFile to first and only Node
+            }
+            else
+            {
+               currentFile.next = new Node(dataFile, null); //Set next
+               currentFile = currentFile.next; //Move up currentFile
+            }
+        }
     }
     public void printStorage()
     {
-        //for(file f : fileArray)
-        //{
-            //print files
-        //}
+        Node iter = firstFile;
+        
+        while(iter != null)
+        {
+            System.out.printf("%d\n", iter.file);
+            
+            //Move iter Node up. If next == null, iter will become null
+            iter = iter.next;
+        }
+    }
+    class Node
+    {
+        int file;
+        Node next;
+        
+        public Node(int file, Node next)
+        {
+            this.file = file;
+            this.next = next;
+        }
     }
 
     @Override
