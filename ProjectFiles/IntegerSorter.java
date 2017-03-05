c462ad33b8336f3efaca3c5c94851ff1b2b3f060
Michael Hewitt <mdhewitt97@gmail.com>
2017-03-05 14:39:57 -0700

    Update IntegerSorter.java

--- ProjectFiles/IntegerSorter.java
+++ ProjectFiles/IntegerSorter.java
@@ -9,13 +9,14 @@ public class IntegerSorter
 	public IntegerSorter(){
 		
 	}
- public void qsort(int[] a, int si, int ei){
+ public double[] qsort(double[] a, int si, int ei){
     //base case
     if(ei<=si || si>=ei){}
 
     else{ 
-        int pivot = a[si]; 
-        int i = si+1; int tmp; 
+        double pivot = a[si]; 
+        int i = si+1; 
+        double tmp; 
 
         //partition array 
         for(int j = si+1; j<= ei; j++){
@@ -35,6 +36,8 @@ public class IntegerSorter
         //call qsort on right and left sides of pivot
         qsort(a, si, i-2); 
         qsort(a, i, ei); 
+        return a;
     }
+    return a;
  }
  }
