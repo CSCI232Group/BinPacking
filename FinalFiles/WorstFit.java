56ae95ed3388d7f82fb8abc53e5bf5caa24d50b7
JacobYakawich <jake.yakawich@gmail.com>
2017-03-05 19:38:46 -0700

    Update WorstFit.java

--- FinalFiles/WorstFit.java
+++ FinalFiles/WorstFit.java
@@ -1,13 +1,12 @@
 package binpacking;
 
-import java.io.File;
+import java.io.File;                                                //Directories that we will be using in this file
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
+/*
  * @author Ian Hecker, Michael Hewitt, Jake Yakowich
  * CSCI 232
  * LAB 2
@@ -17,18 +16,18 @@ public class WorstFit
     private int d=0;
     private double[] data;
     private int size;
-    private ArrayList<Disk> disks=new ArrayList<Disk>();//arraylist of disks
+    private ArrayList<Disk> disks=new ArrayList<Disk>();                    //arraylist of disks
     
     public WorstFit()
     {                    
     	
-    	disks.add(new Disk());//initialize disks and add first disk
+    	disks.add(new Disk());                                              //initialize disks and add first disk
     	disks.get(d).setID(d);
        
     }
     
     public void worstFitSort(double a)
-    {        //adding next file to the next available disk
+    {                                                                       //adding next file to the next available disk
     	if(!disks.get(d).tryToFill(a)){
     		d++;
     		disks.add(new Disk());
@@ -37,7 +36,7 @@ public class WorstFit
     	}
         
     }
-    public void print(){//printing disks in correct order
+    public void print(){                                                    //printing disks in correct order
     	System.out.println("total disks = "+disks.size());
     	    while(disks.size()>0){
     		Disk greatest=disks.get(0);
@@ -49,7 +48,7 @@ public class WorstFit
     		System.out.print(greatest.getID());
     		double t=(double)greatest.storage()/1000000.0;
     		int space=0;
-    		while(t<1){//printing the correct number of spaces for the length of int
+    		while(t<1){                                                    //printing the correct number of spaces for the length of int
     			t=t*10;
     			space++;
     		}
@@ -60,7 +59,7 @@ public class WorstFit
     		greatest.printStorage();
     		for(int k=0;k<disks.size(); k++){
     			if(greatest.getID()==disks.get(k).getID()){
-    				disks.remove(k);//remove the greatest storage space disk from list after printing it
+    				disks.remove(k);                                        //remove the greatest storage space disk from list after printing it
     			}
     		}
     		disks.trimToSize();
@@ -81,7 +80,7 @@ public class WorstFit
             System.setIn(is);
         
             Scanner scan=new Scanner(is);
-            while(scan.hasNext()){//reading in values from text file
+            while(scan.hasNext()){                                              //reading in values from text file
             input.add(scan.nextInt());
             }
             double[] list=new double[input.size()];
@@ -95,13 +94,13 @@ public class WorstFit
             }
             wf.data=list;
            wf.size=input.size();
-           System.out.println("WORST FIT");//DEBUGGING
+           System.out.println("WORST FIT");                                     //DEBUGGING
            System.out.println("file sizes sum = "+total/1000000+" GB");
            wf.print();
            
         }                
     } 
-    public class Disk implements Comparable<Disk>  //????May be "extends" vs implements????
+    public class Disk implements Comparable<Disk>                               //????May be "extends" vs implements????
     {
         private final double totalStorage;
         private double freeStorage;
@@ -111,34 +110,34 @@ public class WorstFit
 
         public Disk()
         {        
-            setID(getID() + 1); //Enumerates Disks as they are created; Int ID's from 0 up
-            totalStorage = 1.0;//Total storage in Disk. Must convert ints accordingly!!!
+            setID(getID() + 1);                                                 //Enumerates Disks as they are created; Int ID's from 0 up
+            totalStorage = 1.0;                                                 //Total storage in Disk. Must convert ints accordingly!!!
             freeStorage = totalStorage;
             firstFile = null;
         }
-        public boolean tryToFill(double dataInt)//Need to convert int to double
+        public boolean tryToFill(double dataInt)                                //Need to convert int to double
         {
-            double dataFile = dataInt;//Conversion of int to double
+            double dataFile = dataInt;                                          //Conversion of int to double
 
             if(freeStorage - dataFile >= 0)
             {
-                freeStorage -= dataFile;//Subtract dataFile from free space
+                freeStorage -= dataFile;                                         //Subtract dataFile from free space
 
-                if(firstFile == null)//If this is the first dataFile:
+                if(firstFile == null)                                           //If this is the first dataFile:
                 {                
-                    firstFile = new Node(dataFile, null); //Creates first Node
-                    currentFile = firstFile; //Sets currentFile to first and only Node
+                    firstFile = new Node(dataFile, null);                    //Creates first Node
+                    currentFile = firstFile;                                     //Sets currentFile to first and only Node
                 }
                 else
                 {                
-                    currentFile.next = new Node(dataFile, null); //Set next                
-                    currentFile = currentFile.next; //Move up currentFile
+                    currentFile.next = new Node(dataFile, null);                //Set next                
+                    currentFile = currentFile.next;                             //Move up currentFile
                 }
                 return true;
             }
             return false;
         }
-        public double convertToInt(int dataFile)//Converts int to double
+        public double convertToInt(int dataFile)                                 //Converts int to double
         {
             return (double)dataFile / 1000000;
         }
@@ -150,7 +149,7 @@ public class WorstFit
 
                 total=total+((int)(iter.file*1000000));
 
-                //Move iter Node up. If next == null, iter will become null
+                                                                                //Move iter Node up. If next == null, iter will become null
                 iter = iter.next;
             }
             return 1000000-total;
@@ -163,7 +162,7 @@ public class WorstFit
 
                 System.out.print((int)(iter.file*1000000)+", ");
 
-                //Move iter Node up. If next == null, iter will become null
+                                                                              //Move iter Node up. If next == null, iter will become null
                 iter = iter.next;
             }
             System.out.println();
@@ -179,8 +178,8 @@ public class WorstFit
                 this.next = next;
             }
         }
-        @Override //This is how the Max Queue compares Disks!! Crucial to function
-        public int compareTo(Disk that)//returns comparison integer
+        @Override                                                           //This is how the Max Queue compares Disks!! Crucial to function
+        public int compareTo(Disk that)                                     //returns comparison integer
         {
             return Double.compare(this.freeStorage, that.freeStorage);
         }
