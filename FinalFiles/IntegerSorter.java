edf35847586d275f651a57c58e25200974e2847b
JacobYakawich <jake.yakawich@gmail.com>
2017-03-05 19:34:26 -0700

    Update IntegerSorter.java

--- FinalFiles/IntegerSorter.java
+++ FinalFiles/IntegerSorter.java
@@ -1,14 +1,16 @@
-package binpacking;
-// Authors Ian Hecker, Michael Hewitt, Jake Yakowich
-// CSCI 232
-// LAB 2
+package binpacking;								//Project BinPacking - Lab 2
+/*
+*Authors Ian Hecker, Michael Hewitt, Jake Yakowich
+*LAB 2
+*CSCI 232
+*/
 public class IntegerSorter
 {
 	public IntegerSorter(){
 		
 	}
- public double[] qsort(double[] a, int si, int ei){//take in parameters for an array, first index, and last index
-    //base case
+ public double[] qsort(double[] a, int si, int ei){				//take in parameters for an array, first index, and last index
+    										//base case
     if(ei<=si || si>=ei){}
 
     else{ 
@@ -16,8 +18,8 @@ public class IntegerSorter
         int i = si+1; 
         double tmp; 
 
-        //partition array 
-        for(int j = si+1; j<= ei; j++){//create partition
+        									//partition array 
+        for(int j = si+1; j<= ei; j++){						//create partition
             if(pivot < a[j]){
                 tmp = a[j]; 
                 a[j] = a[i]; 
@@ -27,11 +29,11 @@ public class IntegerSorter
             }
         }
 
-        //put pivot in right position
+        									//put pivot in right position
         a[si] = a[i-1]; 
         a[i-1] = pivot; 
 
-        //call qsort on right and left sides of pivot
+        									//call qsort on right and left sides of pivot
         qsort(a, si, i-2); 
         qsort(a, i, ei); 
         return a;
